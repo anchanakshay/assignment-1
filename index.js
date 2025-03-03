ce8ddf625fe711368c10273dc01174097cf6cfaa
Akshay Anchan <akshay.anchan@niveussolution.com>
2025-03-03 14:20:03 +0530

    second commit

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ const message = () => {
     .then((message) => console.log(message))
     .catch((err) => console.log(err));
   
+    const fs = require("fs").promises;
+
+const createAFile = async () => {
+  try {
+    await fs.writeFile(
+      "sampleFile.txt",
+      "Good evening...."
+    );
+    console.log("file created successfully");
+  } catch (error) {
+    console.log("something went wrong ", error);
+  }
+};
+
+createAFile();
